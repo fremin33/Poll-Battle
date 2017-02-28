8e4ca9d91f647d3536625ce266851841c5459825
laurentdivet <laurentdivet@icloud.com>
2017-02-28 11:21:55 +0100

    add bouton home pagestart now

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,9 @@
 Rails.application.routes.draw do
-  get 'votes/new'
-
-  get 'votes/create'
-
-  get 'polls/index'
-
-  get 'polls/show'
-
-  get 'categories/index'
-
-  get 'categories/show'
-
   devise_for :users
   mount Attachinary::Engine => "/attachinary"
   root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :categories
+  resources :polls
+  resources :votes
 end
