c353d3bf87b0c02fd8de9b71b156f079ac073ed2
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-02-28 17:21:05 +0100

    Add vote system

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,4 +1,9 @@
 class VotesController < ApplicationController
   def create
+    @response = Response.find(params[:response_id])
+    @vote = @response.votes.new()
+    @vote.user = current_user
+    @vote.save
+    redirect_to poll_path(@response.poll)
   end
 end
