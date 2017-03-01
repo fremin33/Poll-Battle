f498dcf5635057b68b55edfbc40770fb9a47c798
florian fremin <florian.fremin33@gmail.com>
2017-03-01 15:40:44 +0100

    finish form to create a poll

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -1,6 +1,6 @@
 class PollsController < ApplicationController
-  before_action :set_poll, only: [ :show ]
-  skip_before_action :authenticate_user!, only: [:show]
+  before_action :set_poll, only: [:show ]
+  skip_before_action :authenticate_user!, only: [:show, :new]
 
   def show
     @vote = Vote.new
@@ -11,12 +11,38 @@ class PollsController < ApplicationController
     @second_response_count = @second_response.votes.size
   end
 
+  def new
+    @poll = Poll.new
+    @response = @poll.responses.new
+    @categories = Category.all
+  end
+
   def create
+    @categories = Category.all
+    @poll = Poll.new(poll_params)
+    @poll.user = current_user
+    @poll.responses << [Response.new(response_params_1), Response.new(response_params_2)]
+    if @poll.save
+      redirect_to poll_path(@poll)
+    else
+      render :new
+    end
   end
 
   private
 
   def set_poll
     @poll = Poll.find(params[:id])
+  end
+
+  def poll_params
+    params.require(:poll).permit(:title, :category_id)
+  end
+
+  def response_params_1
+    params.require(:poll).require(:response_1).permit(:label, :picture)
+  end
+  def response_params_2
+    params.require(:poll).require(:response_2).permit(:label, :picture)
   end
 end
