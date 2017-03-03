cbff738abfd6e4c8eaa210c4ac15c71423cf29c9
laurentdivet <laurentdivet@icloud.com>
2017-03-03 16:55:06 +0100

    changement photo

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,26 +25,25 @@ Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1
 Category.new(name: "Fashion").save
 Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488366727/fashion_mmgzk9.jpg"
 Category.new(name: "Lifestyle").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488471600/12_lifestyle_anqsgr.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554414/12_lifestyle_gjmymh.jpg"
 Category.new(name: "Car").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488472758/sls_uzo195.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554417/sls_ccu50q.jpg"
 Category.new(name: "Compagny").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488474014/petronas-twin-towers_eumgie.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554416/petronas-twin-towers_zaapp3.jpg"
 Category.new(name: "Ship").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488473149/jet_xgq2kc.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554414/jet_zauoa3.jpg"
 Category.new(name: "Travel").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488473796/travel_n7qszh.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554415/travel_yqw0nj.jpg"
 Category.new(name: "Brand").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488473296/brand-logos-thumbs-up_zcyeam.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554415/brand-logos-thumbs-up_phhucs.jpg"
 Category.new(name: "Motorbike").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488472972/bf070a52edc5f06637efc74cf7fc9258_bm6sz2.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554416/bf070a52edc5f06637efc74cf7fc9258_fsoq3v.jpg"
 Category.new(name: "Technology").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488473723/government-technology_mzzbcs.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554416/government-technology_ildjhq.jpg"
 Category.new(name: "Celebrity").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488473569/eve-arnold-marylin-LA_px7aqt.jpg"
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554415/eve-arnold-marylin-LA_pazgba.jpg"
 Category.new(name: "Television").save
-Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488473403/television_xjhfzh.jpg"
-
+Category.last.picture_url = "http://res.cloudinary.com/dibgaxttc/image/upload/v1488554416/television_zieqaf.jpg"
 
 
 Poll.new(title: "A vs B", user: User.all.sample, category: Category.all.sample).save
