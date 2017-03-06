2321e222347d3a42e4042414ccccab80eb874caa
Pierre Laroze <pierre.larozeprive@gmail.com>
2017-03-06 14:21:11 +0100

    Add facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'votes/create'
 
-  devise_for :users
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   mount Attachinary::Engine => "/attachinary"
   root to: 'pages#home'
 
