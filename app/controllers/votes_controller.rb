f92baaacb923d6f4f47780fc1cb2a5b89c47f79f
florian fremin <florian.fremin33@gmail.com>
2017-03-08 17:28:57 +0100

    add ajax

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -11,7 +11,21 @@ class VotesController < ApplicationController
       @vote = @response.votes.new()
       @vote.user = current_user
     end
-    @vote.save
-    redirect_to poll_path(@response.poll)
+    if @vote.save
+      set_intances_var
+      respond_to do |format|
+        format.js
+        format.html {redirect_to poll_path(@response.poll)}
+      end
+    end
+  end
+  private
+
+  def set_intances_var
+    @f_count = @response.poll.responses.first.votes.size
+    @s_count = @response.poll.responses.last.votes.size
+    total_count = @f_count + @s_count
+    @f_calcul = (@f_count.to_f / total_count.to_f) * 100
+    @s_calcul = (@s_count.to_f / total_count.to_f) * 100
   end
 end
